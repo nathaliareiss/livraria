43f600663614fe8a51bcad1bcceb3c7816efe0fa
Nathalia <alencarnrr@gmail.com>
2026-04-22 13:51:16 -0300

    erros

--- backend/src/routes/booksExternalRoutes.js
+++ backend/src/routes/booksExternalRoutes.js
@@ -1,9 +1,8 @@
 import express from "express";
 import { buscarLivros } from "../controllers/booksExternalController.js";
-import authMiddleware from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.get("/books/search", authMiddleware, buscarLivros);
+router.get("/books/search", buscarLivros);
 
 export default router;
